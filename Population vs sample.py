f481b1cab73d96a99d35c815dc1a114e35b97c75
Akshata Kambar <kambaranil957@gmail.com>
2026-02-06 23:19:42 +0530

    Population VS Sample in Statistics

---
+++ Population vs sample.py
@@ -0,0 +1,8 @@
+## Statistics: 1.Population mean 2.Sample mean 3.Random variable
+# In random variable we have two types 1.Discrete Random variable and Continuous Random variable
+# mean, meadian, mode are the measure of central tendency
+# population: for example to find average height of all the people of a Karnataka state
+# consider all the people oin the state as complete population if we calculate average height use the mean concept of all the people
+# Sample: for example take sample of population in state out of all population we find the average height of the sample population
+# we calculated it by sample mean
+# Sample will basically used in exit polls which party will win the election from the use reporters, news, channels.
