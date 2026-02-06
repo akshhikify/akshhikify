adc07f80287528f53a621a58b3495e3929587d8c
Akshata Kambar <kambaranil957@gmail.com>
2026-02-06 22:44:29 +0530

    Statistics introduction

---
+++ statistics introduction.py
@@ -0,0 +1,7 @@
+## Statistics : Statistics is the discipline that concerns the collection, organizations, analysis, interpretation, and presentation of data.
+# Types : 1.descriptive stats   2.inferential stats
+# 1. Descriptive stats: analyzing data, summarizing data, organizing data in the form of numbers and graphs
+# Examples : bar graphs,Histogram,pie chart,PDF, CDF,normal distribution
+# measure of central tendency  : Mean, meadian, mode
+# Measure of variance: standard deviation , variance
+# 2: Inferential stats: exampled
