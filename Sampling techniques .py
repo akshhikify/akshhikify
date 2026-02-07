185804816d327e5d62dfdb01e4f4871bdcef1739
Akshata Kambar <kambaranil957@gmail.com>
2026-02-07 23:49:26 +0530

    Sampling techniques in statistics

---
+++ Sampling techniques .py
@@ -0,0 +1,9 @@
+## Sampling techniques
+#  1.Simple random sampling  2.stratified sampling  3.systematic sampling  4.convenience sampling
+# 1.simple random sampling: when performing simple random sampling every member of the population (N) has an equal number of chance of being selected for your sample(n).
+# 2.Stratified sample: it is technique where the population(N) is split into non overlapping groups(strata)
+##ex: gender,age etc
+# 3.systematic sampling for ex in mall checking covid for every 8th person it is survey
+# 4.convenience sampling: for example the people who are interested in data science this is selecting people this is convenience sampling.
+### Variables: A variable is a property that can take on any values
+# for example: Height=6.12 and weight=55.5
