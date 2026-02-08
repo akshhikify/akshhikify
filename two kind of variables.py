765f0f686ff8585f85a5cb433aa93a195111f8a0
Akshata Kambar <kambaranil957@gmail.com>
2026-02-08 22:01:55 +0530

    Two kind of variables

---
+++ two kind of variables.py
@@ -0,0 +1,13 @@
+## 1.Quantitative variable:Measure numerically,{Add, substraction , multiple, divides}
+# ex: age ,weight, height
+## 2.Qualitative/categorical variable:
+# ex: gender and IQ
+# In quantitative two type 1.Discrete variable 2.continuous variable
+# 1.ex:whole numbers 2.ex:height and weight
+# Variable measurement scales: 4 types of measure variable
+#1.nominal 2.ordinal 3.Internal 4.ratio
+#1.nominal data: eg:color,gender,type of flower
+#2.ordinal:order of the data matters, value does not
+#3.interval: order matters, value matters, natural zero is not present
+# ex:temperatures
+#4.ratio data: ex: frequency distribution.
