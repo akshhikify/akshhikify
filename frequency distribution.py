9ebd0efd1a5b636d1283e3dfdc08f5e95cd866ff
Akshata Kambar <kambaranil957@gmail.com>
2026-02-09 22:54:38 +0530

    Frequency distribution

---
+++ frequency distribution.py
@@ -0,0 +1,22 @@
+## Frequency distribution: for example: dataset=rose, lilly, sunflower, rose, lilly, sunflower, rose, lilly, lilly
+##  flower      frequency
+#   rose        3
+#   lilly       4
+#   sunflower   5
+# from this frequency table we derive bar graphd, pi charts
+# we have cumulative frequency
+##  flower      frequency       cumulative frequency
+#   rose        3               3
+#   lilly       4               7
+#   sunflower   2               9
+# 1.Bar graphs 2.Histograms
+# 2.Histograms : pdf=smoothing if histogram and it is used for continuous
+# 1.Bar graphs: used for discrete
+
+
+# arithmetic mean for population and sample:
+#         population(N)                            sample(n)
+#      X={1,1,2,2,3,3,4,5,5,6}
+#      mean=sumation i=1 to N into Xi/N           X=sumation i=1 to N into xi/N
+# central tendency: 1.mean  2.meadian  3.mode
+#     refers to the measure used to determine the centre of the distribution of data.
