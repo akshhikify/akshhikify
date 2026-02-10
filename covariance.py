bf45cced5cd11c97c6207fedbed8e4de2f438611
Akshata Kambar <kambaranil957@gmail.com>
2026-02-10 22:24:59 +0530

    covariance

---
+++ covariance.py
@@ -0,0 +1,11 @@
+##  covariance: one of the very important topic when we consider data preprocessing  or data analysis
+#                size                     price
+#                1200sqm                  100R
+#                1500sqm                  200R
+#                1800sqm                  300R
+# Quantifier relationship b/w size and price
+# [cov(size,price)=1/n sumation ofi=1 to n (Xi-Xx)*(Yi-My)]
+# cov()= + * + = +
+# cov()= - * - = +
+# cov()= + * - = -
+# cov()= _ * + = -
