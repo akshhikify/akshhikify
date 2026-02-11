1de5b775ce1c7b538b2f5e58302642723a3afe87
Akshata Kambar <kambaranil957@gmail.com>
2026-02-11 22:27:52 +0530

    mean, median, mode

---
+++ mean, median, mode.py
@@ -0,0 +1,7 @@
+# mean, median, mode
+# mean= sum of number/ total number
+# meadian: first arrange in sorted order ,take central number
+#### for even count of number take two central number
+#### for odd  number take central number
+# mode :maximum number of frequent number of particular distribution
+# ex:: age 
