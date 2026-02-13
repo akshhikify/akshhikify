5f23e6c294a1869b7e87b8b9f4202a0234d0ef9c
Akshata Kambar <kambaranil957@gmail.com>
2026-02-13 23:18:06 +0530

    Intermediate states

---
+++ basics to intermediate states.py
@@ -0,0 +1,10 @@
+## Basics to intermediate states
+# 1. measure of central tendency
+# 2. means of dispersion
+# 3. gaussian distribution
+# 4. Z score
+# 5.standard normal distribution
+### measure of distribution
+#1.variance
+#2.standard deviation
+# population variance and sample variance
