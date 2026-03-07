6747092180dc9bfbbcbc26d1478c65a424e9b9a5
Akshata Kambar <kambaranil957@gmail.com>
2026-03-07 22:22:19 +0530

    Add files via upload

---
+++ javacode.java
@@ -0,0 +1,30 @@
+class Hello
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello, World!");
+        System.out.println(3+4);
+        System.out.println("8+7");
+        int num1=3;
+        int num2=4;
+        int result=num1+num2;
+        System.out.println(result);
+        int num3=9;
+        byte by=127;
+        short sh=558;
+        long l=5854l;
+        float f=5.8f;
+        double d=5.8;
+        char c='k';
+        boolean b=true;
+        System.out.println(num3);
+        System.out.println(by);
+        System.out.println(sh);
+        System.out.println(l);
+        System.out.println(f);
+        System.out.println(d);  
+        System.out.println(c);
+        System.out.println(b);
+    }
+
+}
