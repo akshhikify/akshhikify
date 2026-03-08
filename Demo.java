f7de5356220bca490a34899af1f7938cebbce717
Akshata Kambar <kambaranil957@gmail.com>
2026-03-08 20:45:08 +0530

    Add files via upload

---
+++ Demo.java
@@ -0,0 +1,18 @@
+public class Demo
+{
+   public static void main(String[] args)
+   {
+        int x = 8;
+        int y = 7;
+        int z = 9;
+
+        if(x>10)
+            System.out.println("hello");
+        if(x>5 && x<20)
+            System.out.println("hi");
+        else if(y>z)
+            System.out.println("Welcome");
+        else 
+            System.out.println("Bye");
+   } 
+}
