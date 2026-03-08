125a638e89a08019c53996019387c79f9c9de4d0
Akshata Kambar <kambaranil957@gmail.com>
2026-03-08 21:11:59 +0530

    Ternary operators

---
+++ code.java
@@ -0,0 +1,11 @@
+public class code   // Ternary operator
+{
+    public static void main(String[] args) 
+    {
+        int n = 4;
+        int result = 0;
+        result =n%2==0 ?10:20;
+        System.out.println(result);
+    }
+    
+}
