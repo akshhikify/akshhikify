8b555841fad16067ff71986a9637558aab1d72e0
Akshata Kambar <kambaranil957@gmail.com>
2026-03-09 19:03:59 +0530

    Add files via upload

---
+++ Calculator.java
@@ -0,0 +1,19 @@
+class Calculat{
+    int a;
+
+    public int add(int n1, int n2)
+    {
+        int r = n1 + n2;
+        return r;
+    }
+}
+public class Calculator {
+    public static void main(String[] args) 
+    {
+        int num1 = 4;
+        int num2 = 5;
+        Calculat calc = new Calculat();
+        int result = calc.add(num1, num2);
+        System.out.println(result);
+    }
+}
